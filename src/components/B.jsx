cad5a81ebd40a2ff44c3cccdab5bd2f26a214223
LoganjwSim <j1sim234@gmail.com>
2023-11-30 13:29:30 +0900

    Final

--- src/components/B.jsx
+++ src/components/B.jsx
@@ -2,7 +2,7 @@ import ImageComp from "./ImageComp";
 
 const B = () => {
   return (
-    <div className="bg-blue-100 min-h-screen flex flex-col justify-center items-center gap-96">
+    <div className="bg-blue-100 min-h-screen flex flex-col justify-center items-center gap-20">
       <ImageComp animateName="animate__bounce" image="h662.png" />
       <ImageComp animateName="animate__bounce" image="dd.png" />
     </div>
